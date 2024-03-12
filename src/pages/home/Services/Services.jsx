5c87d945ef2b8db713e50a28c5b76c510ac33d93
AlamNurul95 <nurulalamsm95@gmail.com>
2024-03-12 13:53:06 +0000

    12march commit2

---
+++ src/pages/home/Services/Services.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Services = () => {
+    return (
+       
+           
+            
+            <div>
+            <div>
+                <div className="flex items-center">
+      <div className="border-t border-black flex-grow mr-4"></div>
+      <div className="text-center"><h2>hshshshsh</h2></div>
+      <div className="border-t border-black flex-grow ml-4"></div>
+    </div>
+            </div>
+            </div>
+            
+            
+    
+            
+        
+    );
+};
+
+export default Services;
