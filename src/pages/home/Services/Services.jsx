822208f9e6f164e4bae930215b09f7fe2e608ec8
AlamNurul95 <nurulalamsm95@gmail.com>
2024-03-13 09:48:51 +0000

    13march-1st

--- src/pages/home/Services/Services.jsx
+++ src/pages/home/Services/Services.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Cards from './Cards';
 
 const Services = () => {
     return (
@@ -9,9 +10,14 @@ const Services = () => {
             <div>
                 <div className="flex items-center">
       <div className="border-t border-black flex-grow mr-4"></div>
-      <div className="text-center"><h2>hshshshsh</h2></div>
+      <div className="text-center"><h2>Our Best Packages</h2></div>
       <div className="border-t border-black flex-grow ml-4"></div>
-    </div>
+                </div>
+            </div>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 my-6'>
+            {
+                <Cards></Cards>
+            }
             </div>
             </div>
             
